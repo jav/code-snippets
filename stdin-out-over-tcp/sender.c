d17c66f6318770087859c2de07150acd4ddcee06
Javier Ubillos <javvee@gmail.com>
2011-04-28 15:41:56 +0200

    Simplified somewhat (removed outputs to file) and looks allright, need receiver to test further.

--- stdin-out-over-tcp/sender.c
+++ stdin-out-over-tcp/sender.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <errno.h>
+#include <arpa/inet.h>
+#include <unistd.h>
 #include "gopt.h"
 
 #define BUFLEN 2
@@ -12,22 +15,30 @@ int main(int argc, const char **argv)
 	int loopc=0; /* number of loop iterations */
 	char inbuf[BUFLEN+1]; /* Input-buffer */
 	const char *in_filename =0; 
-	const char *out_filename =0; 
-	const char *log_filename =0; 
-	FILE *fout, *fin, *flog;
+	const char *str_address, *str_port, *str_address_family;
+	char str_ip4_addr[INET_ADDRSTRLEN];
+	FILE *fin;
+	int fd_out;
+	int af=0;
+	struct sockaddr *sa;
+	struct sockaddr_in ip4_sa;
+
 
 	void *options= gopt_sort( &argc, argv, gopt_start(
 					  gopt_option( 'h', 0, gopt_shorts( 'h', '?' ), gopt_longs( "help", "HELP" )),
-					  gopt_option( 'l', GOPT_ARG, gopt_shorts( 'l' ), gopt_longs( "log" )),
-					  gopt_option( 'o', GOPT_ARG, gopt_shorts( 'o' ), gopt_longs( "out" )), 
-					  gopt_option( 'i', GOPT_ARG, gopt_shorts( 'i' ), gopt_longs( "in" )) 
+					  gopt_option( 'i', GOPT_ARG, gopt_shorts( 'i' ), gopt_longs( "in" )), 
+					  gopt_option( 'd', GOPT_ARG, gopt_shorts( 0 ), gopt_longs( "dest", "destination", "address" )),
+					  gopt_option( 't', GOPT_ARG, gopt_shorts( 0 ), gopt_longs( "address_family", "af" )),
+					  gopt_option( 'p', GOPT_ARG, gopt_shorts( 'p' ), gopt_longs( "port" )) 
 					  ));
 
 	if( gopt( options, 'h' ) ) {
 		/*
 		 * if any of the help options was specified
 		 */
-		fprintf( stdout, "HELP!\n" );
+		fprintf( stdout, "Usage: sender [-i file| --in file] \n" );
+		fprintf( stdout, "              outputs to stdout\n" );
+		fprintf( stdout, "Report bugs to <javier at ubillos.org>.\n" );
 		exit( EXIT_SUCCESS );
 	}
 	
@@ -35,7 +46,7 @@ int main(int argc, const char **argv)
 	if ( gopt_arg( options, 'i', &in_filename ) && strcmp( in_filename, "-" ) ) {
 		fin = fopen( in_filename, "r" );
 		if( ! fin ) {
-			fprintf(stderr, "Could not open input (-i/--in) file.");
+			fprintf(stderr, "Could not open input (-i/--in) file.\n");
 			exit( EXIT_FAILURE );
 		}
 	}
@@ -44,31 +55,50 @@ int main(int argc, const char **argv)
 		fin = stdin;
 	}
 
+/* OUTPUT 
+ *     and -n is specified -> network
+ *         check that we have set -f, -a, -p
+ *         if( -f AF_INET && -a && -p) -> IPv4
+ * else, -o/-out is not specified
+ *     set stdout as fd_out
+ */
         /* If we have specified an output file, or it is specified to "-" */
-	if ( gopt_arg( options, 'o', &out_filename ) && strcmp( out_filename, "-" ) ) {
-		fout = fopen( out_filename, "wb" );
-		if( ! fout ) {
-			fprintf(stderr, "Could not open output (-o/--out) file.");
+	if( !gopt_arg( options, 'd', &str_address ) || 
+	    !gopt_arg( options, 'p', &str_port ) || 
+	    !gopt_arg( options, 't', &str_address_family) ) {
+		fprintf(stderr, "To use \"network\" as an output, --dest, --port and --af need to be specified\n");
+		exit( EXIT_FAILURE );	
+	}
+	/* OUT-INET */
+	
+	
+	if( 0 == strcmp("AF_INET", str_address_family) ) {
+		ip4_sa.sin_family = af = AF_INET;
+		memset(&ip4_sa, 0, sizeof(ip4_sa));
+		sa = (struct sockaddr *) &ip4_sa;
+		printf("str_address: %s\n", str_address);
+		inet_pton(af, str_address, &(ip4_sa.sin_addr));
+		inet_ntop(af, &(ip4_sa.sin_addr), str_ip4_addr, INET_ADDRSTRLEN);
+		
+		ip4_sa.sin_port = htons(atoi(str_port));
+		printf("Connecting to %s : %s\n", str_ip4_addr, str_port);
+		if( 0 > (fd_out = socket(AF_INET, SOCK_STREAM, 0)) ) {
+			perror("ERROR opening socket\n");
 			exit( EXIT_FAILURE );
 		}
-	}
-        /* If we have not specified an output file, or it is specified to "-" */
-	else { 
-		fout = stdout;
-	}
-
-        /* If we have specified an log/error-output file, or it is specified to "-" */
-	if ( gopt_arg( options, 'l', &log_filename ) && strcmp( log_filename, "-" ) ) {
-		flog = fopen( log_filename, "wb" );
-		if( ! flog ) {
-			fprintf(stderr, "Could not open log/err-output (-l/--log) file.");
+		
+		printf("Socket opened, connecting to %s : %s\n", str_address, str_port);
+		if( 0 > connect(fd_out, (struct sockaddr *)&ip4_sa, sizeof(ip4_sa) ) ) {
+			perror("ERROR connecting socket\n");
 			exit( EXIT_FAILURE );
 		}
+		printf("Socket connected\n");
 	}
-        /* If we have not specified an log/error-output file, or it is specified to "-" */
-	else { 
-		flog = stderr;
+	else {
+		fprintf(stderr, "Address family: %s is not a supported address family\n", str_address_family);
+		exit( EXIT_FAILURE );	
 	}
+	
 	
 	/* Prepare (zero) the data-buffer */
 	memset(inbuf, 0, BUFLEN+1);
@@ -77,15 +107,15 @@ int main(int argc, const char **argv)
 	do {
 		/* Output to stderr (or log) */
 		loopc +=1;
-		fprintf(flog, "loopc: %d\n", loopc);
+		fprintf(stderr, "loopc: %d\n", loopc);
 		nc += i;
-		fprintf(flog, "Read characters: i:%d, %s\n", i, inbuf);
+		fprintf(stderr, "Read characters: i:%d, %s\n", i, inbuf);
 		/* Output to stdout */
-		fwrite(inbuf, sizeof(char), i, fout);
+		write(fd_out, inbuf, i);
 	}
 	while (BUFLEN == (i = fread(inbuf, sizeof(char), BUFLEN, fin)  ) ) ;
 
-	fprintf(flog, "Counted characters: %d\n", nc);
+	fprintf(stderr, "Counted characters: %d\n", nc);
 
 	return EXIT_SUCCESS;
 }
