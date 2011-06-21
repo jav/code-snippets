a24b6454d73237a145502188d2dcc6e5ea68b3c6
jav <jav@debian.(none)>
2011-06-21 10:02:49 +0200

    Trivial Tcp6 client/server.

--- tcp6/client.c
+++ tcp6/client.c
@@ -3,6 +3,7 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <netdb.h> 
+#include <resolv.h>
 
 void error(char *msg)
 {
@@ -13,7 +14,7 @@ void error(char *msg)
 int main(int argc, char *argv[])
 {
     int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
+    struct sockaddr_in6 serv_addr;
     struct hostent *server;
 
     char buffer[256];
@@ -21,21 +22,41 @@ int main(int argc, char *argv[])
        fprintf(stderr,"usage %s hostname port\n", argv[0]);
        exit(0);
     }
+
+    bzero((char *) &serv_addr, sizeof(serv_addr));
+    serv_addr.sin6_family = AF_INET6;
+
     portno = atoi(argv[2]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    sockfd = socket(AF_INET6, SOCK_STREAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
-    server = gethostbyname(argv[1]);
+
+    // Is it a 'raw' IPv6 address?
+    if (inet_pton(AF_INET6, argv[1], &serv_addr.sin6_addr) > 0) {
+	    
+	    //note inet_pton will take care of setting the address
+	    
+	    serv_addr.sin6_family = AF_INET6;
+
+    }
+    else {
+    
+	    //res init is defined in resolv.h 
+	    res_init();
+	    _res.options |= RES_USE_INET6;
+	    server = gethostbyname(argv[1]);
+	    bcopy((char *)server->h_addr, 
+		  (char *)&serv_addr.sin6_addr.s6_addr,
+		  server->h_length);
+    }
+
     if (server == NULL) {
         fprintf(stderr,"ERROR, no such host\n");
         exit(0);
     }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
+
+    
+    serv_addr.sin6_port = htons(portno);
     if (connect(sockfd,&serv_addr,sizeof(serv_addr)) < 0) 
         error("ERROR connecting");
     printf("Please enter the message: ");
--- tcp6/server.c
+++ tcp6/server.c
@@ -15,20 +15,20 @@ int main(int argc, char *argv[])
 {
      int sockfd, newsockfd, portno, clilen;
      char buffer[256];
-     struct sockaddr_in serv_addr, cli_addr;
+     struct sockaddr_in6 serv_addr, cli_addr;
      int n;
      if (argc < 2) {
          fprintf(stderr,"ERROR, no port provided\n");
          exit(1);
      }
-     sockfd = socket(AF_INET, SOCK_STREAM, 0);
+     sockfd = socket(AF_INET6, SOCK_STREAM, 0);
      if (sockfd < 0) 
         error("ERROR opening socket");
      bzero((char *) &serv_addr, sizeof(serv_addr));
      portno = atoi(argv[1]);
-     serv_addr.sin_family = AF_INET;
-     serv_addr.sin_addr.s_addr = INADDR_ANY;
-     serv_addr.sin_port = htons(portno);
+     serv_addr.sin6_family = AF_INET6;
+     serv_addr.sin6_addr = in6addr_any;
+     serv_addr.sin6_port = htons(portno);
      if (bind(sockfd, (struct sockaddr *) &serv_addr,
               sizeof(serv_addr)) < 0) 
               error("ERROR on binding");
